82ca05141d99a2c36abfe07f6d0cb038b80fee2b
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-09-05 00:56:52 +0100

    Explicitly import <cstdint> to fix GCC builds
    stricter than LLVM I guess

--- src/CubemapData.h
+++ src/CubemapData.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <array>
 #include <memory>
+#include <cstdint>
 
 namespace Fate {
     struct CubemapData {
--- src/Vertex.h
+++ src/Vertex.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <cstdint>
 
 namespace Fate {
     struct Vertex {
